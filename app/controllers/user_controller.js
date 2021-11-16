14f9ab997101b6be29b981d059197b302217ef32
Travis Brewer <t.brewer.co@gmail.com>
2021-11-16 14:55:09 -0700

    adds get all users

--- app/controllers/user_controller.js
+++ app/controllers/user_controller.js
@@ -3,20 +3,37 @@ const User = db.users;
 const Op = db.Sequelize.Op;
 
 // create user
-exports.create = (req, res) => {
-    User.create({
-        username: req.body.username,
-        given_name: req.body.given_name,
-        family_name: req.body.family_name,
-        role: req.body.role,
-        email: req.body.email,
-        password: req.body.password,
-        profile_photo_url: req.body.profile_photo_url
-    }).then(user => {
-        res.send(user)
-    }).catch(error => {
+exports.create = async (req, res) => {
+    try {
+        const user = await User.create({
+            username: req.body.username,
+            given_name: req.body.given_name,
+            family_name: req.body.family_name,
+            role: req.body.role,
+            email: req.body.email,
+            password: req.body.password,
+            profile_photo_url: req.body.profile_photo_url
+        });
+        res.send(user);
+    } catch (error) {
         res.send({
             "Error": error.errors[0].message
-        })
-    })
+        });
+    };
 };
+
+// get all users
+exports.findAll = async (req, res) => {
+    try {
+        const users = await User.findAll();
+        res.send(users);
+    } catch (error) {
+        res.send(error)
+    };
+};
+
+// get a user
+
+// update user
+
+// delete user
