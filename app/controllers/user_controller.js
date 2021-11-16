85b5617600a3f338129abedc165aa00467803f8b
Travis Brewer <t.brewer.co@gmail.com>
2021-11-16 14:20:20 -0700

    adds create user

--- app/controllers/user_controller.js
+++ app/controllers/user_controller.js
@@ -2,50 +2,21 @@ const db = require("../models");
 const User = db.users;
 const Op = db.Sequelize.Op;
 
-// create and save new user
+// create user
 exports.create = (req, res) => {
-    // validate request
-    if (!req.body.username) {
-        res.status(400)("Content cannot be empty.")
-    };
-    return;
-};
-
-// create a user
-// const user = {
-//     username: req.body.username,
-//     given_name: req.body.given_name,
-//     family_name: req.body.family_name,
-//     email: req.body.email,
-//     password: req.body.password,
-//     profile_photo_url: req.body.profile_photo_url
-// };
-
-// // save tutorial in db
-// User.create(user)
-//     .then(data => {
-//         res.send(data);
-//     })
-//     .catch(error => {
-//         res.status(500).send({
-//             message:
-//                 error.message || "An error occurred while saving user"
-//         });
-//     });
-
-// get all users from db
-exports.findAll = (req, res) => {
-    const username = req.query.username;
-    let condition = title ? { title: { [Op.like]: `%${username}` } } : null;
-
-    User.findAll({ where: condition })
-        .then(data => {
-            res.send(data);
+    User.create({
+        username: req.body.username,
+        given_name: req.body.given_name,
+        family_name: req.body.family_name,
+        role: req.body.role,
+        email: req.body.email,
+        password: req.body.password,
+        profile_photo_url: req.body.profile_photo_url
+    }).then(user => {
+        res.send(user)
+    }).catch(error => {
+        res.send({
+            "Error": error.errors[0].message
         })
-        .catch(error => {
-            res.status(500).send({
-                message:
-                    error.message || "An error occurred while retrieving user"
-            });
-        });
+    })
 };
