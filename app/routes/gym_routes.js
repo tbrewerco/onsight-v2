6f898031c0d2aa4efde818d0b663cc80557dcbd9
Travis Brewer <t.brewer.co@gmail.com>
2021-11-18 15:08:28 -0700

    removes excess characters

--- app/routes/gym_routes.js
+++ app/routes/gym_routes.js
@@ -2,19 +2,19 @@ module.exports = app => {
     const gyms = require("../controllers/gym_controller.js");
     const router = require("express").Router();
 
-    // // create new gym
+    // create new gym
     router.post("/", gyms.create);
 
-    // // get all gyms
+    // get all gyms
     router.get("/", gyms.findAll);
 
-    // // get a gym
+    // get a gym
     router.get("/:id", gyms.findOne);
 
-    // // update gym
+    // update gym
     router.patch("/:id", gyms.update);
 
-    // // delete gym
+    // delete gym
     router.delete("/:id", gyms.delete);
 
     app.use('/api/v1/gyms', router);
