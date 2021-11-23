d294f36bf1266e4b693b9d8f0e34a963894e638d
Travis Brewer <t.brewer.co@gmail.com>
2021-11-23 16:31:37 -0700

    adds mysql-migrations with migration.js, schema.sql

---
+++ migrations/schema.sql
@@ -0,0 +1,260 @@
+-- MySQL dump 10.13  Distrib 8.0.27, for macos11.6 (arm64)
+--
+-- Host: 127.0.0.1    Database: onsight
+-- ------------------------------------------------------
+-- Server version	8.0.27
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `climbing_routes`
+--
+
+DROP TABLE IF EXISTS `climbing_routes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `climbing_routes` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `is_top_rope` enum('yes','no') NOT NULL,
+  `is_auto_belay` enum('yes','no') NOT NULL,
+  `is_lead_climb` enum('yes','no') NOT NULL,
+  `is_boulder` enum('yes','no') NOT NULL,
+  `hold_color` varchar(255) NOT NULL,
+  `setter_grade` int DEFAULT NULL,
+  `gym_wall_section_id` int DEFAULT NULL,
+  `setter_id` int DEFAULT NULL,
+  `gym_id` int DEFAULT NULL,
+  `image_url` varchar(255) DEFAULT NULL,
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime NOT NULL,
+  `is_boulder_or_rope_climb` enum('rope','boulder') NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `image_url` (`image_url`),
+  KEY `gym_wall_section_id` (`gym_wall_section_id`),
+  KEY `setter_id` (`setter_id`),
+  KEY `gym_id` (`gym_id`),
+  CONSTRAINT `climbing_routes_ibfk_1` FOREIGN KEY (`gym_wall_section_id`) REFERENCES `gym_wall_sections` (`id`),
+  CONSTRAINT `climbing_routes_ibfk_2` FOREIGN KEY (`setter_id`) REFERENCES `users` (`id`),
+  CONSTRAINT `climbing_routes_ibfk_3` FOREIGN KEY (`gym_id`) REFERENCES `gyms` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `gym_wall_sections`
+--
+
+DROP TABLE IF EXISTS `gym_wall_sections`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `gym_wall_sections` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `gym_id` int DEFAULT NULL,
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`),
+  KEY `gym_id` (`gym_id`),
+  CONSTRAINT `gym_wall_sections_ibfk_1` FOREIGN KEY (`gym_id`) REFERENCES `gyms` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `gyms`
+--
+
+DROP TABLE IF EXISTS `gyms`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `gyms` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `address_street` varchar(255) NOT NULL,
+  `address_city` varchar(255) NOT NULL,
+  `address_state` varchar(255) NOT NULL,
+  `address_zip` varchar(255) NOT NULL,
+  `address_coordinates` point DEFAULT NULL,
+  `has_boulders` enum('yes','no') NOT NULL,
+  `has_sport_routes` enum('yes','no') NOT NULL,
+  `has_auto_belays` enum('yes','no') NOT NULL,
+  `photo_url` varchar(255) DEFAULT NULL,
+  `created_by` int DEFAULT NULL,
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`),
+  UNIQUE KEY `photo_url` (`photo_url`),
+  KEY `created_by` (`created_by`),
+  CONSTRAINT `gyms_ibfk_1` FOREIGN KEY (`created_by`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `mysql_migrations_347ertt3e`
+--
+
+DROP TABLE IF EXISTS `mysql_migrations_347ertt3e`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `mysql_migrations_347ertt3e` (
+  `timestamp` varchar(254) NOT NULL,
+  UNIQUE KEY `timestamp` (`timestamp`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `route_tags`
+--
+
+DROP TABLE IF EXISTS `route_tags`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `route_tags` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `route_id` int DEFAULT NULL,
+  `tag` varchar(255) DEFAULT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `route_id` (`route_id`),
+  CONSTRAINT `route_tags_ibfk_1` FOREIGN KEY (`route_id`) REFERENCES `climbing_routes` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `ticks`
+--
+
+DROP TABLE IF EXISTS `ticks`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ticks` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `comment` varchar(255) DEFAULT NULL,
+  `did_send` enum('yes','no') NOT NULL,
+  `did_flash` enum('yes','no') DEFAULT 'no',
+  `did_onsight` enum('yes','no') DEFAULT 'no',
+  `quality_rating` int DEFAULT NULL,
+  `difficulty_grade` int DEFAULT NULL,
+  `gym_id` int DEFAULT NULL,
+  `route_id` int DEFAULT NULL,
+  `user_id` int DEFAULT NULL,
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `gym_id` (`gym_id`),
+  KEY `route_id` (`route_id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `ticks_ibfk_1` FOREIGN KEY (`gym_id`) REFERENCES `gyms` (`id`),
+  CONSTRAINT `ticks_ibfk_2` FOREIGN KEY (`route_id`) REFERENCES `climbing_routes` (`id`),
+  CONSTRAINT `ticks_ibfk_3` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `user_favorite_gyms`
+--
+
+DROP TABLE IF EXISTS `user_favorite_gyms`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user_favorite_gyms` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int DEFAULT NULL,
+  `gym_id` int DEFAULT NULL,
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  KEY `gym_id` (`gym_id`),
+  CONSTRAINT `user_favorite_gyms_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
+  CONSTRAINT `user_favorite_gyms_ibfk_2` FOREIGN KEY (`gym_id`) REFERENCES `gyms` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `user_favorite_routes`
+--
+
+DROP TABLE IF EXISTS `user_favorite_routes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user_favorite_routes` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int DEFAULT NULL,
+  `route_id` int DEFAULT NULL,
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  KEY `route_id` (`route_id`),
+  CONSTRAINT `user_favorite_routes_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
+  CONSTRAINT `user_favorite_routes_ibfk_2` FOREIGN KEY (`route_id`) REFERENCES `climbing_routes` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `user_ticks`
+--
+
+DROP TABLE IF EXISTS `user_ticks`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user_ticks` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int DEFAULT NULL,
+  `tick_id` int DEFAULT NULL,
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  KEY `tick_id` (`tick_id`),
+  CONSTRAINT `user_ticks_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
+  CONSTRAINT `user_ticks_ibfk_2` FOREIGN KEY (`tick_id`) REFERENCES `ticks` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL,
+  `given_name` varchar(255) NOT NULL,
+  `family_name` varchar(255) NOT NULL,
+  `role` enum('admin','user') NOT NULL DEFAULT 'user',
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `profile_photo_url` varchar(255) DEFAULT NULL,
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`),
+  UNIQUE KEY `profile_photo_url` (`profile_photo_url`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-11-23 15:54:37
