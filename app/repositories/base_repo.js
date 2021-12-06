298604b42ce81ec3a654218219e9df735cdfdba4
Travis Brewer <t.brewer.co@gmail.com>
2021-12-06 15:49:08 -0700

    adds security to base repo:  whitelist and prepared statements

--- app/repositories/base_repo.js
+++ app/repositories/base_repo.js
@@ -6,32 +6,40 @@ exports.findAll = async (tableName) => {
     });
     return new Promise((resolve, reject) => {
         const sql = `SELECT * FROM ${tableName};`;
-        connection.query(sql, (error, result, fields) => {
-            if (error) {
-                return reject("db error: " + error.message);
-            } else {
-                return resolve(result);
-            };
-        });
+        const allowedTables = ["climbing_routes", "gym_wall_sections", "gyms", "route_tags", "ticks"];
+        if (allowedTables.includes(tableName)) {
+            connection.execute(sql, [tableName], (error, result, fields) => {
+                if (error) {
+                    return reject("db error: " + error.message);
+                } else {
+                    return resolve(result);
+                };
+            });
+        } else {
+            throw new Error("db error: invalid query");
+        };
     });
 };
 
 exports.findByAttribute = async (tableName, attribute, reqParams) => {
-    connection.getConnection(function (error) {
-        if (error) throw (error);
-    });
-    return new Promise((resolve, reject) => {
-        const sql = `SELECT * FROM ${tableName} WHERE ${attribute} = ?;`;
-        connection.query(sql, [
-            reqParams
-        ], (error, result) => {
-            if (error) {
-                return reject("db error: " + error.message);
-            } else {
-                return resolve(result);
-            };
+    const allowedTables = ["climbing_routes", "gym_wall_sections", "gyms", "route_tags", "ticks", "user_favorite_gyms", "user_favorite_routes", "user_ticks"];
+    if (allowedTables.includes(tableName)) {
+        connection.getConnection(function (error) {
+            if (error) throw (error);
         });
-    });
+        return new Promise((resolve, reject) => {
+            const sql = `SELECT * FROM ?? WHERE ?? = ?;`;
+            connection.query(sql, [tableName, attribute, reqParams], (error, result) => {
+                if (error) {
+                    return reject("db error: " + error.message);
+                } else {
+                    return resolve(result);
+                };
+            });
+        });
+    } else {
+        throw new Error("db error: invalid query");
+    };
 };
 
 // delete
