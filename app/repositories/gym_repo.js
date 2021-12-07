2826cb292f4e01a5786c042e985a2ab22db92211
Travis Brewer <t.brewer.co@gmail.com>
2021-12-06 17:31:22 -0700

    updates gymrepo create to prepared statements

--- app/repositories/gym_repo.js
+++ app/repositories/gym_repo.js
@@ -5,27 +5,31 @@ exports.create = async (newGym) => {
         if (error) throw (error);
     });
     return new Promise((resolve, reject) => {
-        // NEED TO SANITIZE (USE LIBRARY?)
         const sql = `
         INSERT INTO gyms SET
-        name='${newGym.name}',
-        address_street='${newGym.addressStreet}',
-        address_city='${newGym.addressCity}',
-        address_state='${newGym.addressState}',
-        address_zip='${newGym.addressZip}',
-        address_coordinates=ST_PointFromText('POINT(${newGym.addressCoordinates.x} ${newGym.addressCoordinates.y})'),
-        has_boulders='${newGym.hasBoulders}',
-        has_sport_routes='${newGym.hasSportRoutes}',
-        has_auto_belays='${newGym.hasAutoBelays}',
-        ${newGym.photoUrl ? `photo_url='${newGym.photoUrl}', created_by='${newGym.createdBy}'` : `created_by=${newGym.createdBy}`}
+        name= ?,
+        address_street= ?,
+        address_city= ?,
+        address_state= ?,
+        address_zip= ?,
+        address_coordinates=ST_PointFromText('POINT( ? ? )'),
+        has_boulders= ?,
+        has_sport_routes= ?,
+        has_auto_belays= ?,
+        photo_url= ?, 
+        created_by= ?
         `;
-        connection.query(sql, (error, result, fields) => {
-            if (error) {
-                throw new Error("DB Error: " + error.code);
-            } else {
-                return resolve(result.insertId);
-            };
-        });
+        connection.query(sql,
+            [
+                newGym.name, newGym.addressStreet, newGym.addressCity, newGym.addressState, newGym.addressZip, newGym.addressCoordinates.x, newGym.addressCoordinates.y, newGym.hasBoulders, newGym.hasSportRoutes, newGym.hasAutoBelays, newGym.photoUrl, newGym.createdBy
+            ],
+            (error, result, fields) => {
+                if (error) {
+                    throw new Error("DB Error: " + error);
+                } else {
+                    return resolve(result.insertId);
+                };
+            });
     });
 };
 
