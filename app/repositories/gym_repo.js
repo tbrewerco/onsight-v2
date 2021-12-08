6a8d97df1610ec6638485070286d7514955a08f3
Travis Brewer <t.brewer.co@gmail.com>
2021-12-07 17:07:51 -0700

    dynamically updates sql query based on data

--- app/repositories/gym_repo.js
+++ app/repositories/gym_repo.js
@@ -1,5 +1,20 @@
 const { connection, mysql } = require("../../db/index.js");
 
+const sqlReturnObject = {
+    name: "name= ?,",
+    addressStreet: "address_street= ?,",
+    addressCity: "address_city= ?,",
+    addressState: "address_state= ?,",
+    addressZip: "address_zip= ?,",
+    addressCoordinates: "address_coordinates=ST_PointFromText('POINT( ? ? )'),",
+    hasBoulders: "has_boulders= ?,",
+    hasSportRoutes: "has_sport_routes= ?,",
+    hasAutoBelays: "has_auto_belays= ?,",
+    photoUrl: "photo_url= ?,",
+    createdBy: "created_by= ?,",
+    isDeleted: "is_deleted= ?"
+};
+
 exports.create = async (newGym) => {
     connection.getConnection(function (error) {
         if (error) throw (error);
@@ -33,30 +48,24 @@ exports.create = async (newGym) => {
     });
 };
 
-// update
+
 exports.update = async (data, id) => {
-    console.log(data.name)
-    connection.getConnection(function (error) {
-        if (error) throw (error);
-    });
-    return new Promise((resolve, reject) => {
-        const sql = `
-        UPDATE gyms SET
-        name= ?,
-        address_street= ?,
-        address_city= ?,
-        address_state= ?,
-        address_zip= ?,
-        has_boulders= ?,
-        has_sport_routes= ?,
-        has_auto_belays= ?,
-        photo_url= ?, 
-        created_by= ?
-        WHERE id = ${id}
-        `;
+
+    const getSqlQuery = () => {
+        let sqlArray = [];
+        Object.keys(data).map(prop => {
+            sqlArray.push(sqlReturnObject[prop])
+        });
+        return sqlArray.join('\n').replace(/,\s*$/, "");
+    };
+
+    const sql = `UPDATE gyms SET ${getSqlQuery()} WHERE id = ?;`
+
+    return new Promise((resolve, reject, sqlQuery) => {
         connection.query(sql,
+            // need to dynamically populate this array
             [
-                data.name, data.addressStreet, data.addressCity, data.addressState, data.addressZip, data.hasBoulders, data.hasSportRoutes, data.hasAutoBelays, data.photoUrl, data.createdBy
+                data.name, data.addressStreet, data.addressCoordinates.x, data.addressCoordinates.y, data.hasBoulders, data.hasSportRoutes, data.hasAutoBelays, data.createdBy, id
             ],
             (error, result, fields) => {
                 if (error) {
