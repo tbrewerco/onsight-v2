3e0b60aa61379828e74bbcd3517305b7fd651871
Travis Brewer <78047938+tbrewerco@users.noreply.github.com>
2023-01-19 14:50:27 -0700

    change to snakeCase

--- app/controllers/gymController.js
+++ app/controllers/gymController.js
@@ -7,16 +7,16 @@ exports.create = async (req, res) => {
     try {
         const gym = await Gym.create({
             name: req.body.name,
-            address_street: req.body.address_street,
-            address_city: req.body.address_city,
-            address_state: req.body.address_state,
-            address_zip: req.body.address_zip,
-            address_coordinates: req.body.address_coordinates,
-            has_boulders: req.body.has_boulders,
-            has_sport_routes: req.body.has_sport_routes,
-            has_auto_belays: req.body.has_auto_belays,
-            photo_url: req.body.photo_url,
-            created_by: req.body.created_by
+            address_street: req.body.addressStreet,
+            address_city: req.body.addressCity,
+            address_state: req.body.addressState,
+            address_zip: req.body.addressZip,
+            address_coordinates: req.body.addressCoordinates,
+            has_boulders: req.body.hasBoulders,
+            has_sport_routes: req.body.hasSportRoutes,
+            has_auto_belays: req.body.hasAutoBelays,
+            photo_url: req.body.photoUrl,
+            created_by: req.body.createdBy
         });
         res.status(200).send(gym);
     } catch (error) {
