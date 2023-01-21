89c3c9f98764fdb3746f1213e30bcefc325573dd
Travis Brewer <78047938+tbrewerco@users.noreply.github.com>
2023-01-20 17:21:58 -0700

    wip: define entity associtations in index.js

--- db/index.js
+++ db/index.js
@@ -27,8 +27,31 @@ db.users = require("../app/models/userModel.js")(sequelize, Sequelize);
 db.userFavoriteRoutes = require("../app/models/userFavoriteRouteModel.js")(sequelize, Sequelize);
 db.userFavoriteGyms = require("../app/models/userFavoriteGymModel.js")(sequelize, Sequelize);
 
+// define associations
+// gyms
+db.gyms.hasMany(db.climbingRoutes);
+db.climbingRoutes.belongsTo(db.gyms);
+
+db.gyms.hasMany(db.gymWallSections);
+db.gymWallSections.belongsTo(db.gyms);
+
+// climbing routes
+db.climbingRoutes.hasMany(db.ticks);
+db.ticks.belongsTo(db.climbingRoutes);
+
+db.climbingRoutes.hasMany(db.routeTags);
+db.routeTags.belongsTo(db.climbingRoutes);
+
+// gym wall sections
+db.gymWallSections.hasMany(db.climbingRoutes);
+db.climbingRoutes.belongsTo(db.gymWallSections);
+
+// users
+db.users.hasMany(db.ticks);
+db.ticks.belongsTo(db.users);
+
 // the following will destroy data, so be careful
-sequelize.sync({ force: true });
-console.log("All models were synchronized successfully.");
+// sequelize.sync({ force: true });
+// console.log("All models were synchronized successfully.");
 
 module.exports = db;
